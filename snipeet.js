290165e434939e7fce4a47bc00f852574933332f
ParthGurval <sohamgurval2001@gmail.com>
2023-09-22 23:14:08 +0530

    Practice of code
    Practice of code

--- snipeet.js
+++ snipeet.js
@@ -1,10 +1,10 @@
-function solve(arr, rotations){
-    if(rotations == 0) return arr;
-    for(let i = 0; i < rotations; i++){
-      let element = arr.pop();
-      arr.unshift(element);
-    }
-    return arr;
-   }
+// function solve(arr, rotations){
+//     if(rotations == 0) return arr;
+//     for(let i = 0; i < rotations; i++){
+//       let element = arr.pop();
+//       arr.unshift(element);
+//     }
+//     return arr;
+//    }
 
-   solve([44, 1, 22, 111], 5);
+//    solve([44, 1, 22, 111], 5);
